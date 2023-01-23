d6997a56c45c9194b8434e93e901330842129870
Denis_Vlaskov <denchikbig@gmail.com>
2023-01-23 20:43:44 +0700

    Сделал первые два задания из Домашней работы

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №2. Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+//a = 5; b = 7 -> max =7
+//a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+Console.Write("Введите число a: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA>numberB)
+{
+    Console.WriteLine("Число a является большим числом, число b меньшим");
+}
+else
+{
+    Console.WriteLine("Число b является большим числом, число a меньшим");
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44,5,78 -> 78
+//22, 3, 9, -> 22
+
+Console.Write("Введите число A:");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B:");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите число C:");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+
+if(numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.Write("max=" + max);
