d8da277f97a2dc747b8984ac9a0c4997d9dcc50f
Denis_Vlaskov <denchikbig@gmail.com>
2023-01-29 13:36:39 +0700

    Сделал задание №8

---
+++ Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на входе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+int evenNumber = 2;
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
